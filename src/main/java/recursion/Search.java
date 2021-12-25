4bcee8bacc70cfcf7e3c0169c980fbf2b4694550
Joana Socrates <joana.socrates@ocado.com>
2021-12-25 03:11:14 -0300

    recuisive-search

--- src/main/java/recursion/Search.java
+++ src/main/java/recursion/Search.java
@@ -2,6 +2,7 @@ package recursion;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /* given a reppsitory of key words and a search query
@@ -13,8 +14,26 @@ public class Search {
     List<List<String>> findSuggestions (List<String> repository, String query){
         List<List<String>> result = new ArrayList<>() ;
 
+        Collections.sort(repository);
+
 
 
         return result;
+    }
+
+    int binarySearch(List<String> repository, char x, int intitialIndex, int finalIndex){
+
+        if(repository.get((finalIndex-intitialIndex)/2).charAt(0)==x){
+            return finalIndex-intitialIndex/2;
+        }
+
+        if(repository.get(finalIndex-intitialIndex/2).charAt(0)>x){
+            return binarySearch(repository, x, intitialIndex, finalIndex-intitialIndex)/2)
+        }
+
+        if(repository.get(finalIndex-intitialIndex/2).charAt(0)<x){
+            return binarySearch(repository, x, intitialIndex, finalIndex-intitialIndex)/2)
+        }
+
     }
 }
